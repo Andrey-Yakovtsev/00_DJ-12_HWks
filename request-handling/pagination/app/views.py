486549d0680b65b7749a155c0f3f05f0b6d06815
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-08-29 12:35:09 +0300

    Переделал пагинацию, но как вывести последнюю страницу, если нет содержимого в запросе?

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
@@ -8,7 +10,6 @@ import csv
 def index(request):
     return redirect(reverse(bus_stations))
 
-DATA = [str(i) for i in range (10000)]
 def bus_stations(request):
     stations_list = []
     with open('data-398-2018-08-30.csv', encoding='cp1251', newline='') as csvfile:
@@ -18,19 +19,11 @@ def bus_stations(request):
     paginator = Paginator(stations_list, 5)
     current_page = request.GET.get('page', 1)
     page_obj = paginator.get_page(current_page)
-    # msg = '<br>'.join(page_obj.object_list)
-    if page_obj.has_next():
-        page_obj.next_page_number()
-    if page_obj.has_previous():
-        page_obj.previous_page_number()
-    next_page_url = f'bus_stations?page={page_obj.next_page_number()}'
-    previous_page_url = f'bus_stations?page={page_obj.previous_page_number()}'
+
     return render_to_response('index.html', context={
         'bus_stations': page_obj,
         'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
+        'prev_page_url': f'bus_stations?page={int(current_page)-1}' if page_obj.has_previous() else None,
+        'next_page_url': f'bus_stations?page={page_obj.next_page_number()}'
     })
 
-def get_data_from_csv(request):
-    pass
