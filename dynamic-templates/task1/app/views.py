0b0d413032d4988d892ec8c4f81cd648762a7cae
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-08-30 16:52:31 +0300

    TASK 1 Completed

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,27 @@
+import csv
+
+
 from django.shortcuts import render
 
 def inflation_view(request):
     template_name = 'inflation.html'
+    with open("inflation_russia.csv", encoding="utf-8") as fo:
+        read_csv = list(csv.reader(fo))
+    table = [x.split(';') for b in read_csv for x in b]
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
+    for array in table:
+        for i in range(len(array)):
+            old_value = array[i]
+            try:
+                if old_value == '':
+                    new_value = '-'
+                    array[i] = new_value
+            except:
+                pass
 
-    return render(request, template_name,
-                  context)
+    context = {
+        'table_header': table[0],
+        'table': table[1:]
+    }
+
+    return render(request, template_name, context)
