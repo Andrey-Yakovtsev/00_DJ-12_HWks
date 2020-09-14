7928db3f9022fefcb9d2c2fedb51e8c782e5a675
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-09-14 19:29:40 +0300

    Django-DB-1-2 - in progress

--- databases/phone_comparison/phones/admin.py
+++ databases/phone_comparison/phones/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Phone, Apple_brand, Samsung_brand, Nokia_brand
 
-# Register your models here.
+
+class PhoneAdmin(admin.ModelAdmin):
+    pass
+
+
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(Apple_brand, BrandAdmin)
+admin.site.register(Samsung_brand, BrandAdmin)
+admin.site.register(Nokia_brand, BrandAdmin)
--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Phone(models.Model):
+    modelname = models.TextField(primary_key=True, serialize=False)
+    brand = models.TextField(blank=True)
+    name = models.TextField(blank=False)
+    price = models.FloatField(blank=True)
+    image = models.ImageField(blank=True)
+    model_year = models.CharField(max_length=4, blank=True)
+    has_camera = models.BooleanField(blank=True)
+    camera_res = models.CharField(max_length=50, blank=True)
+    sim_type = models.TextField(blank=True)
+    dimensions = models.CharField(max_length=50, blank=True)
+    weight = models.CharField(max_length=10, blank=True)
+    battery_capacity = models.CharField(max_length=30, blank=True)
+    os_version = models.TextField(blank=True)
+    processor = models.TextField(blank=True)
+    gps_support = models.TextField(blank=True)
+    lte_exists = models.BooleanField(blank=True)
+    fingerprint_scanner = models.BooleanField(blank=True)
+    face_scanner = models.BooleanField(blank=True)
+    color = models.TextField(blank=True)
+    slug = models.CharField(max_length=150, blank=True, default='')
+
+
+class Apple_brand(models.Model):
+    model = models.ForeignKey(Phone, default='', on_delete=models.CASCADE, verbose_name='Название модели', related_name='+')
+    display = models.CharField(max_length=100, default='', blank=True, null=True)
+    giroscope = models.BooleanField(default=True, null=True)
+    connection = models.CharField(max_length=100, default='', blank=True, null=True)
+
+
+class Samsung_brand(models.Model):
+    model = models.ForeignKey(Phone, default='', on_delete=models.CASCADE, verbose_name='Название модели', related_name='+')
+    sound = models.TextField(default='')
+    sd_card_support = models.BooleanField(default=True)
+
+
+class Nokia_brand(models.Model):
+    model = models.ForeignKey(Phone, default='', on_delete=models.CASCADE, verbose_name='Название модели', related_name='+')
+    GPU = models.CharField(max_length=50)
