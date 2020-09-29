211b957071aff3a556b262d5deaf52214bfe925c
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-09-29 14:52:45 +0300

    DB2-2 - done

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -6,7 +6,8 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    students_data = Student.objects.prefetch_related().order_by('group')
+    # students_data = Student.objects.all().order_by('group')
+    students_data = Student.objects.prefetch_related('teacher').all().order_by('group')
     context = {'object_list': students_data}
 
     return render(request, template, context)
